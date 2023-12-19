a7849ab37a4cf5bc84e2b464432aaf2d30b40bd8
阮文詠 <145126166+vinhintw@users.noreply.github.com>
2023-12-19 21:02:07 +0800

    Update Main.java

--- Main.java
+++ Main.java
@@ -8,7 +8,6 @@ public class Main{
     public static void main(String[] args) {
        String[] s = InputReader.readInput();
         MRT myMrt = new MRT();
-        myMrt.loadData();
         for (int i = 0; i < myMrt.myData.size(); i++) {
             if ((myMrt.myData.get(i).getStart() == s[0] && myMrt.myData.get(i).getEnd() == s[1]) || (myMrt.myData.get(i).getStart() == s[1] && myMrt.myData.get(i).getEnd() == s[0]) ) {
                 System.out.println(myMrt.myData.get(i));
